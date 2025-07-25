4a48b64a151599a2741ffdbb24ffb882be015af3
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-07-25 15:21:56 -0300

    some tests

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Singastore!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://universe.leagueoflegends.com/en_PL/champion/singed/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Singas Lore
         </a>
       </header>
     </div>
