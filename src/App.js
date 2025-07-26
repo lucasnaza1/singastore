b500df2dff5491864265159c9755748e9958959a
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-07-26 13:38:39 -0300

    header created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Singastore!
-        </p>
-        <a
-          className="App-link"
-          href="https://universe.leagueoflegends.com/en_PL/champion/singed/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Singas Lore
-        </a>
+        <div className='logo'>
+        <img src={logo} alt= 'Singastore logo'></img>
+        <p>SingaStore</p>
+        </div>
       </header>
     </div>
   );
