20d9be6d59401516517fdd60f15d6cf3fbcd47eb
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-08-04 16:48:00 -0300

    Add Pesquisa and Input components to app
    Introduced a new Pesquisa component with styled title, subtitle, and input field. Created a reusable styled Input component. Updated App.js to render the Pesquisa component below the Header.

---
+++ src/components/Input/index.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+        border: 1px solid #FFF;
+        background: transparent;
+        border: 1px solid #FFF;
+        padding: 10px 120px;
+        border-radius: 25px;
+        width: 200px;
+        color: #FFF;
+        font-size: 16px;
+        margin-bottom: 12px;
+
+        &::placeholder {
+                color: #FFF;
+                font-size: 16px;
+        }
+`
+
+export default Input
---
+++ src/components/Pesquisa/index.js
@@ -0,0 +1,38 @@
+import Input from "../Input";
+import styled from "styled-components";
+
+const PesquisaContainer = styled.section`
+        background-image: linear-gradient(90deg, #145315ff 35%, #49824aff 165%);
+        color: #FFF;
+        text-align: center;
+        padding: 85px 0;
+        height: 270px;
+        width: 100%;
+`
+
+const Titulo = styled.h2`
+        color: #FFF;
+        font-size: 36px;
+        text-align: center;
+        width: 100%;
+`
+
+const Subtitulo = styled.h3`
+        ont-size: 16px;
+        font-weight: 500;
+        margin-bottom: 40px;
+`
+
+function Pesquisa() {
+    return(
+        <PesquisaContainer>
+            <Titulo>Mexer, ou não mexer?</Titulo>
+            <Subtitulo>Nossas poções são batidas. Nâo misturadas.</Subtitulo>
+            <Input
+                placeholder='Que tal um drink?'/>
+        </PesquisaContainer>
+      
+    )
+}
+
+export default Pesquisa;
