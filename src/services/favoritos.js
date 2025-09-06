b64a4898c4a8c68ca5d941ecc9c8779c5ef36b48
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-09-06 13:33:17 -0300

    post and delete favoritos created on services

--- src/services/favoritos.js
+++ src/services/favoritos.js
@@ -8,6 +8,16 @@ async function getFavoritos() {
     return response.data
 }
 
+async function postFavoritos(id) {
+    await favoritosAPI.post(`/${id}`)
+}
+
+async function deleteFavoritos(id) {
+    await favoritosAPI.delete(`/${id}`)
+}
+
 export {
-    getFavoritos
+    getFavoritos,
+    postFavoritos,
+    deleteFavoritos
 }   
